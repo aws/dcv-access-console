af944a2e02188a4a2b6839bf013d59d3a0ed45d6
Shivansh Mittal <>
2025-01-28 23:47:36 +0000

    fix: Upgrade next.js and postcss versions

--- dcv-access-console-web-client/server/next-env.d.ts
+++ dcv-access-console-web-client/server/next-env.d.ts
@@ -1,6 +1,3 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: Apache-2.0
-
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
