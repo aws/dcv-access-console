42333e0840db58c181d3d53a4f0daac118aabc3d
Aria Stroud <ustraria@amazon.com>
2025-09-25 13:09:39 -0700

    chore: Updates copyright year to 2025

--- dcv-access-console-web-client/server/src/constants/dcv-horizontal-split-layout-constants.ts
+++ dcv-access-console-web-client/server/src/constants/dcv-horizontal-split-layout-constants.ts
@@ -2,5 +2,5 @@
 // SPDX-License-Identifier: Apache-2.0
 
 export const DCV_HORIZONTAL_SPLIT_LAYOUT_CONSTANTS = {
-    COPYRIGHT_MESSAGE: "© 2024, Amazon Web Services, Inc. or its affiliates. All rights reserved"
+    COPYRIGHT_MESSAGE: "© 2025, Amazon Web Services, Inc. or its affiliates. All rights reserved"
 }
